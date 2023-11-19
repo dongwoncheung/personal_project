7b7803587138377b4009fe27086777150c2946e1
학원 <test@test.com>
2023-11-19 20:22:53 +0900

    초기 환경세팅

---
+++ src/main/java/com/project/test/TestController.java
@@ -0,0 +1,41 @@
+package com.project.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+public class TestController {
+	
+//	@Autowired
+//	PostMapper postMapper;
+	@ResponseBody
+	@GetMapping("/test1")
+	public String test1() {
+		return "hello world";
+	}
+	@ResponseBody
+	@GetMapping("/test2")
+		public Map<String, Object>test2(){
+			Map<String, Object> map = new HashMap<>();
+			map.put("a", 1);
+			map.put("b", 200);
+			return map;
+		}
+	@GetMapping("/test3")
+	public String test3() {
+		return"test/test";
+	}
+//	@ResponseBody
+//	@GetMapping("/test4")
+//	public List<Map<String, Object>> test4() {
+//		return postMapper.selectPostList();
+//	}
+}
